5667086f5c2058953b40c86d2c3939f049175d31
hadinishad <hadinishad42@gmail.com>
2016-05-30 16:25:04 +0530

    Support for move to vtop

--- autocaptcha-for-firefox/lib/main.js
+++ autocaptcha-for-firefox/lib/main.js
@@ -2,12 +2,12 @@
 var pageMod = require("sdk/page-mod");
 var self = require("sdk/self");
 pageMod.PageMod({
-  include: ["https://academics.vit.ac.in/parent/parent_login.asp","https://academics.vit.ac.in/student/stud_login.asp","https://academics.vit.ac.in/parent/","https://academics.vit.ac.in/student/","https://27.251.102.132/parent/parent_login.asp","https://27.251.102.132/student/stud_login.asp","https://27.251.102.132/parent/","https://27.251.102.132/student/"],
+  include: ["https://vtop.vit.ac.in/parent/parent_login.asp","https://vtop.vit.ac.in/student/stud_login.asp","https://vtop.vit.ac.in/parent/","https://vtop.vit.ac.in/student/","https://27.251.102.132/parent/parent_login.asp","https://27.251.102.132/student/stud_login.asp","https://27.251.102.132/parent/","https://27.251.102.132/student/"],
   contentScriptFile: self.data.url("captcha.js")
 });
 
 pageMod.PageMod({
-  include: /http(s)+:\/\/(academics.vit.ac.in)|(27.251.102.132)\/(.)*\/attn_report.asp(.)*/,
+  include: /http(s)+:\/\/(vtop.vit.ac.in)|(27.251.102.132)\/(.)*\/attn_report.asp(.)*/,
   contentScriptFile: self.data.url("attendance.js"),
   attachTo: ["existing","top","frame"]
 });
