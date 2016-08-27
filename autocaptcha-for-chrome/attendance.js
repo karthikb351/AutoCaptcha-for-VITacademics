f872fb33d5da7ee941431c436982692f6d64bae4
cybergla <tanaydeshmukh@gmail.com>
2016-08-27 11:04:12 +0530

    Added start date to attendance.js
    The term start date now appears in the 'from_date' field.

--- autocaptcha-for-chrome/attendance.js
+++ autocaptcha-for-chrome/attendance.js
@@ -7,4 +7,7 @@ function getDateString(date) {
 			date.getFullYear();
 }
 
+var dt = document.evaluate( '/html/body/table/tbody/tr/td[2]/table/tbody/tr[3]/td[3]/font', document, null, XPathResult.STRING_TYPE, null);
+
+document.getElementsByName("from_date")[0].value=dt.stringValue;
 document.getElementsByName("to_date")[0].value=getDateString(new Date());
